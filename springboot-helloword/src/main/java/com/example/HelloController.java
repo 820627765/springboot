ba4638fc88fdea6ba9ca7d1dfe08f7fd26322fed
820627765 <820627765@qq.com>
2021-08-11 07:29:55 +0800

    'update'

--- springboot-helloword/src/main/java/com/example/HelloController.java
+++ springboot-helloword/src/main/java/com/example/HelloController.java
@@ -4,11 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class HelloController {
+    private List<Byte[]> bytes = new ArrayList<>();
     @ResponseBody
     @GetMapping("/hello")
     public String hello() {
+        return "hello Word";
+    }
+
+    @ResponseBody
+    @GetMapping("/hello1")
+    public String hello1() {
+        bytes.add(new Byte[1024 * 1024 * 10]);
         return "hello Word";
     }
 }
